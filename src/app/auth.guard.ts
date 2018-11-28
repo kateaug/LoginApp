364f841d90259d570c672bbfc27c9abee1117d91
Kasia <k.augustyniakk@gmail.com>
2018-11-28 20:02:12 +0100

    removing unused component

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -18,7 +18,7 @@ export class AuthGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-      //debugger
+      
       if(this.authorizationService.isLoggedIn) {
         return true
       }
